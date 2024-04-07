4fd9e24b07ef06e2b71d81a81d9ef9ca347632bf
emylyano3 <emiliano.schiano@gmail.com>
2024-04-07 10:54:04 -0300

    channels

--- src/main.cpp
+++ src/main.cpp
@@ -3,25 +3,25 @@
 #include <ESP32Domotic.h>
 #include <Logger.h>
 
-ESP32Domotic  domoticModule;
-Channel       channel("c1", "channel_1", 1, OUTPUT, false, true);
+ESP32Domotic*  domoticModule  = new ESP32Domotic();
+Channel*       channel        = new Channel("c1", "channel_1", 1, OUTPUT, false, true);
 
 void setup() {
   Serial.begin(115200);
   log("Switch example setup");
-  channel.setState(LOW);
-  channel.setTimer(20);
-  domoticModule.setModuleType("switch");
-  domoticModule.setWifiConnectTimeout(20);
-  domoticModule.setConfigPortalTimeout(120);
-  domoticModule.setPortalSSID("proeza-domotics-demo");
-  domoticModule.addChannel(&channel);
-  domoticModule.init();
+  channel->setState(LOW);
+  channel->setTimer(20);
+  domoticModule->setModuleType("switch");
+  domoticModule->setWifiConnectTimeout(20);
+  domoticModule->setConfigPortalTimeout(120);
+  domoticModule->setPortalSSID("proeza-domotics-demo");
+  domoticModule->addChannel(channel);
+  domoticModule->init();
 
-  log("Channel is analog", channel.isAnalog());
-  log("Channel is output", channel.isOutput());
-  log("Module location", domoticModule.getConfig()->getModuleLocation());
-  log("Module name", domoticModule.getConfig()->getModuleName());
+  log("Channel is analog", channel->isAnalog());
+  log("Channel is output", channel->isOutput());
+  log("Module location", domoticModule->getConfig()->getModuleLocation());
+  log("Module name", domoticModule->getConfig()->getModuleName());
 }
 
 void loop() {
@@ -29,6 +29,6 @@ void loop() {
   // log("Channel previous state", channel.getPrevState());
   // log("Channel current raw state", channel.getRawState());
   // log("Channel current mapped state", channel.getMappedState());
-  domoticModule.cycle();
-  delay(1000);
+  domoticModule->cycle();
+  delay(100);
 }
